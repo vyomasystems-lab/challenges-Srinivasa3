e35d6a88954345b5b27f00c674701e6b624bb2be
Srinivasa3 <tsrinivasa59@gmail.com>
2022-07-24 07:49:37 +0000

    update

--- vyoma.sh
+++ vyoma.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+​
 export PS1='\[\033[01;32m\]vyoma\[\033[00m\] \[\033[01;34m\]\w\[\033[00m\] (uptickpro) $ '
 brew install icarus-verilog
 PYTHON_CONFIGURE_OPTS="--enable-shared" pyenv install 3.8.13
@@ -11,6 +11,6 @@ cp -r /workspace/.pyenv_mirror/user/3.8.13/lib/python3.8/site-packages/cocotb*  
 cp -r /workspace/.pyenv_mirror/user/3.8.13/lib/python3.8/site-packages/pygpi /home/gitpod/.pyenv/versions/3.8.13/lib/python3.8/site-packages/
 rm -rf core.*
 clear
-echo "****** UpTickPro (Evaluation Version) 1.0.0 *******"
+echo "** UpTickPro (Evaluation Version) 1.0.0 ***"
 echo "Copyright (c) 2022, Vyoma Systems Private Limited"
 echo "All Rights Reserved."
