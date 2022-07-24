6dca6a814dad803c8f700ffbfa63b227d9d48c5a
Srinivasa3 <tsrinivasa59@gmail.com>
2022-07-24 08:18:25 +0000

    up

--- level1_design1/test_mux.py
+++ level1_design1/test_mux.py
@@ -6,5 +6,9 @@ from cocotb.triggers import Timer
 @cocotb.test()
 async def test_mux(dut):
     """Test for mux2"""
+    dut.sel.value=0b00011;
+    dut.inp3.value=0b11; 
 
-    cocotb.log.info('##### CTB: Develop your test here ########')
+    //cocotb.log.info('##### CTB: Develop your test here ########')
+    dut._log.info(f'sel={dut.sel.value:05}  inp={dut.inp3.value:05} out={dut.out.value:05}')
+    assert dut.inp3.value == dut.out.value, "Error"
