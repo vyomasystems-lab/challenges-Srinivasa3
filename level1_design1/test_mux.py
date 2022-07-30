d1ad6c48e48a0db8c4cab5ddf974fe0dbc2a898d
Srinivasa3 <tsrinivasa59@gmail.com>
2022-07-30 19:49:02 +0000

    5

--- level1_design1/test_mux.py
+++ level1_design1/test_mux.py
@@ -12,11 +12,10 @@ async def test_mux(dut):
                 temp=0b11
             else:
                 temp=0b00
-            print(temp)
             try :
                 assert temp == dut.out.value, "success"
             except AssertionError:
-                print ("FAIL Either slection or input is missing")
+                print ("FAIL:  input missing")
             return temp
             
    ##         await Timer(2, units='ns')
@@ -29,7 +28,122 @@ async def test_mux(dut):
     dut.sel.value=0b00001
     dut.inp1.value=0b11
     await Timer(2, units='ns')
-    call(temp)
-
-
+    temp=call(temp)
+     dut.sel.value=0b00010
+    dut.inp0.value=0b00
+    await Timer(2, units='ns')
+    temp=call(temp)
+    dut.sel.value=0b00011
+    dut.inp1.value=0b11
+    await Timer(2, units='ns')
+    temp=call(temp) 
+    dut.sel.value=0b00100
+    dut.inp0.value=0b00
+    await Timer(2, units='ns')
+    temp=call(temp)
+    dut.sel.value=0b00101
+    dut.inp1.value=0b11
+    await Timer(2, units='ns')
+    temp=call(temp) 
+    dut.sel.value=0b00110
+    dut.inp0.value=0b00
+    await Timer(2, units='ns')
+    temp=call(temp)
+    dut.sel.value=0b00111
+    dut.inp1.value=0b11
+    await Timer(2, units='ns')
+    temp=call(temp) 
+    dut.sel.value=0b01000
+    dut.inp0.value=0b00
+    await Timer(2, units='ns')
+    temp=call(temp)
+    dut.sel.value=0b01001
+    dut.inp1.value=0b11
+    await Timer(2, units='ns')
+    temp=call(temp) 
+    dut.sel.value=0b01010
+    dut.inp0.value=0b00
+    await Timer(2, units='ns')
+    temp=call(temp)
+    dut.sel.value=0b01011
+    dut.inp1.value=0b11
+    await Timer(2, units='ns')
+    temp=call(temp) 
+    dut.sel.value=0b01100
+    dut.inp0.value=0b00
+    await Timer(2, units='ns')
+    temp=call(temp)
+    dut.sel.value=0b01101
+    dut.inp1.value=0b11
+    await Timer(2, units='ns')
+    temp=call(temp) 
+    dut.sel.value=0b01110
+    dut.inp0.value=0b00
+    await Timer(2, units='ns')
+    temp=call(temp)
+    dut.sel.value=0b01111
+    dut.inp1.value=0b11
+    await Timer(2, units='ns')
+    temp=call(temp) 
+    dut.sel.value=0b10000
+    dut.inp0.value=0b00
+    await Timer(2, units='ns')
+    temp=call(temp)
+    dut.sel.value=0b10001
+    dut.inp1.value=0b11
+    await Timer(2, units='ns')
+    temp=call(temp) 
+    dut.sel.value=0b10010
+    dut.inp0.value=0b00
+    await Timer(2, units='ns')
+    temp=call(temp)
+    dut.sel.value=0b10011
+    dut.inp1.value=0b11
+    await Timer(2, units='ns')
+    temp=call(temp) 
+    dut.sel.value=0b10100
+    dut.inp0.value=0b00
+    await Timer(2, units='ns')
+    temp=call(temp)
+    dut.sel.value=0b10101
+    dut.inp1.value=0b11
+    await Timer(2, units='ns')
+    temp=call(temp) 
+    dut.sel.value=0b10110
+    dut.inp0.value=0b00
+    await Timer(2, units='ns')
+    temp=call(temp)
+    dut.sel.value=0b10111
+    dut.inp1.value=0b11
+    await Timer(2, units='ns')
+    temp=call(temp) 
+    dut.sel.value=0b11000
+    dut.inp0.value=0b00
+    await Timer(2, units='ns')
+    temp=call(temp)
+    dut.sel.value=0b11001
+    dut.inp1.value=0b11
+    await Timer(2, units='ns')
+    temp=call(temp) 
+    dut.sel.value=0b11010
+    dut.inp0.value=0b00
+    await Timer(2, units='ns')
+    temp=call(temp)
+    dut.sel.value=0b11011
+    dut.inp1.value=0b11
+    await Timer(2, units='ns')
+    temp=call(temp) 
+    dut.sel.value=0b11100
+    dut.inp0.value=0b00
+    await Timer(2, units='ns')
+    temp=call(temp)
+    dut.sel.value=0b11101
+    dut.inp1.value=0b11
+    await Timer(2, units='ns')
+    temp=call(temp) 
+    dut.sel.value=0b11110
+    dut.inp0.value=0b00
+    await Timer(2, units='ns')
+    temp=call(temp)
+    
 
