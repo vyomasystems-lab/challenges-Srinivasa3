94558606a06682acf9393b788f68df4df73084c6
Srinivasa3 <tsrinivasa59@gmail.com>
2022-07-30 19:01:42 +0000

    2

--- level1_design1/test_mux.py
+++ level1_design1/test_mux.py
@@ -7,17 +7,19 @@ from cocotb.triggers import Timer
 async def test_mux(dut):
     """Test for mux2"""
     def call():
-    if temp==0b00:
-        temp=0b11
-    else:
-        temp=0b00
-    await Timer(2, units='ns')
-    try :
-        assert temp == dut.out.value, "success"
-    except AssertionError:
-        print ("FAIL Either slection or input is missing")
+            try :
+                assert temp == dut.out.value, "success"
+            except AssertionError:
+                print ("FAIL Either slection or input is missing")
+            if temp==0b00:
+                temp=0b11
+            else:
+                temp=0b00
+            await Timer(2, units='ns')
+    
     dut.sel.value=0b00000
-    dut.inp0.value=0b11
+    dut.inp0.value=0b00
+    temp=0b00
     await Timer(2, units='ns')
     call()
 
