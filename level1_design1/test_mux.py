5c61161e89472ace1a3da1a5460b744d8f55f289
Srinivasa3 <tsrinivasa59@gmail.com>
2022-07-31 14:36:24 +0000

    3

--- level1_design1/test_mux.py
+++ level1_design1/test_mux.py
@@ -3,14 +3,7 @@
 import cocotb
 from cocotb.triggers import Timer
 
-@cocotb.test()
-async def test_mux(dut):
-    """Test for mux0"""
-    dut.sel.value = 0b00000
-    dut.inp0.value = 0b10
-    await Timer(2, units='ns')
-    dut._log.info(f'Sel = {(dut.sel.value)} Input= {(dut.inp0.value)}, DUT={(dut.out.value)}')
-    assert dut.inp0.value == dut.out.value, "Output is not matching"
+
 
 @cocotb.test()
 async def test_mux(dut):
@@ -20,4 +13,11 @@ async def test_mux(dut):
     await Timer(2, units='ns')
     dut._log.info(f'Sel = {(dut.sel.value)} Input= {(dut.inp2.value)}, DUT={(dut.out.value)}')
     assert dut.inp2.value == dut.out.value, "Output is not matching"
-
+@cocotb.test()
+async def test_mux(dut):
+    """Test for mux0"""
+    dut.sel.value = 0b00000
+    dut.inp0.value = 0b10
+    await Timer(2, units='ns')
+    dut._log.info(f'Sel = {(dut.sel.value)} Input= {(dut.inp0.value)}, DUT={(dut.out.value)}')
+    assert dut.inp0.value == dut.out.value, "Output is not matching"
