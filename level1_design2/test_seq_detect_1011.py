0531088baf679d8b359045ddb48981069805c250
Srinivasa3 <tsrinivasa59@gmail.com>
2022-08-01 10:27:05 +0000

    up

--- level1_design2/test_seq_detect_1011.py
+++ level1_design2/test_seq_detect_1011.py
@@ -11,7 +11,7 @@ from cocotb.clock import Clock
 from cocotb.triggers import RisingEdge, FallingEdge
 
 @cocotb.test()
-async def test_seq_bug1(dut):
+async def test_seq_1(dut):
     """Test for seq detection """
 
     clock = Clock(dut.clk, 10, units="us")  # Create a 10us period clock on port clk
@@ -19,8 +19,43 @@ async def test_seq_bug1(dut):
 
     # reset
     dut.reset.value = 1
-    await FallingEdge(dut.clk)  
-    dut.reset.value = 0
-    await FallingEdge(dut.clk)
+    await FallingEdge(dut.clk) 
+    dut.inp_bit.value = 1
+    await FallingEdge(dut.clk) 
+    dut._log.info(f"Inp = {dut.inp_bit.value} State = {dut.current_state.value} Out = {dut.seq_seen.value}")
+    assert dut.current_state.value == 0b000, "Reset is not working" 
+    assert dut.seq_seen.value == 0, "Design error"
 
-    cocotb.log.info('#### CTB: Develop your test here! ######')
+@cocotb.test()
+async def test_seq_2(dut):
+    
+    clock = Clock(dut.clk, 10, units="us")  # Create a 10us period clock on port clk
+    cocotb.start_soon(clock.start())        # Start the clock
+
+    # reset
+    dut.reset.value = 1
+    await FallingEdge(dut.clk) 
+    dut.reset.value = 0
+    dut.inp_bit.value = 0
+    await FallingEdge(dut.clk) 
+    dut._log.info(f"Inp = {dut.inp_bit.value} State = {dut.current_state.value} Out = {dut.seq_seen.value}")
+    assert dut.current_state.value == 0b000, "Reset is not working" 
+    assert dut.seq_seen.value == 0, "Design error"
+
+@cocotb.test()
+async def test_seq_3(dut):
+    
+    clock = Clock(dut.clk, 10, units="us")  # Create a 10us period clock on port clk
+    cocotb.start_soon(clock.start())        # Start the clock
+
+    # reset
+    dut.reset.value = 1
+    await FallingEdge(dut.clk) 
+    dut.reset.value = 0
+    dut.inp_bit.value = 0
+    await FallingEdge(dut.clk)
+    dut.inp_bit.value = 1
+    await FallingEdge(dut.clk)
+    dut._log.info(f"Inp = {dut.inp_bit.value} State = {dut.current_state.value} Out = {dut.seq_seen.value}")
+    assert dut.current_state.value == 0b001, "State transition failed" 
+    assert dut.seq_seen.value == 0, "Design error"
