63acaf7e9c5ebb99097eaa381e0dfc5732c7a7d2
Srinivasa3 <tsrinivasa59@gmail.com>
2022-08-01 11:35:39 +0000

    update

--- level1_design2/seq_detect_1011_pass/seq_detect_1011_pass.v
+++ level1_design2/seq_detect_1011_pass/seq_detect_1011_pass.v
@@ -46,7 +46,7 @@ module seq_detect_1011(seq_seen, inp_bit, reset, clk);
       SEQ_1:
       begin
         if(inp_bit == 1)
-          next_state = SEQ_1; //State changed to SEQ_1 
+          next_state = SEQ_1; //State changed to SEQ_1 - fixed
         else
           next_state = SEQ_10;
       end
@@ -62,12 +62,12 @@ module seq_detect_1011(seq_seen, inp_bit, reset, clk);
         if(inp_bit == 1)
           next_state = SEQ_1011;
         else
-          next_state = SEQ_10; //State changed to SEQ_10 
+          next_state = SEQ_10; //State changed to SEQ_10 - fixed
       end
       SEQ_1011:
       begin
         if(inp_bit == 1)
-          next_state = SEQ_1;
+          next_state = SEQ_1; // Additional state added - fixed
         else
           next_state = IDLE;
       end
